b18f2310c5b7e5514124005254b3a31307fe2a24
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2022-07-04 10:03:18 +0200

    Remove warning when no SIMD is found

--- src/ScanByte.jl
+++ src/ScanByte.jl
@@ -6,12 +6,6 @@ using SIMD: SIMD
 include("byteset.jl")
 include("codegen.jl")
 
-function __init__()
-    if DEFVEC === nothing
-        @warn "SIMD capacity not detected by ScanByte, using scalar fallback"
-    end
-end
-
 export ByteSet, memchr
 
 end # module
